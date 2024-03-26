430ecc1d43335864a228b562cbea04ff008754ad
Mohammad Munaf <mohammadmehfooz100@gmail.com>
2024-03-26 15:20:03 +0530

    add date to each product

--- models/item.js
+++ models/item.js
@@ -1,26 +1,30 @@
-const mongoose=require('mongoose');
-const itemSchema=mongoose.Schema({
-    name:{
-        type:String,
-        required:true,
+const mongoose = require('mongoose');
+const itemSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
     },
-    description:{
-        type:String,
-        required:true
+    description: {
+        type: String,
+        required: true
     },
-    url:{
-        type:String,
-        required:true
+    url: {
+        type: String,
+        required: true
     },
-    price:{
-        type:Number
+    price: {
+        type: Number
     },
-    category:{
-        type:String,
-        required:true
+    category: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
     }
 })
-const Item=mongoose.model("Item",itemSchema);
+const Item = mongoose.model("Item", itemSchema);
 // const firstItem=new Item({name:"car",description:"sadasdas",url:"jasdas",price:130});
 // firstItem.save();
-module.exports=Item;
+module.exports = Item;
